4b723d0d90c42a6a862816022de0991269cc7e6c
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-01-30 15:25:13 +0000

    Clean proxy API

--- proxy_API/proxy.rb
+++ proxy_API/proxy.rb
@@ -6,41 +6,21 @@ class App < Sinatra::Application
 
   class APIRequest
     include HTTParty
-    # base_uri "http://index1.homeflow.co.uk"
-
 
     def places(name)
-      # self.class.get("/places?api_key=77467477edfd2689cd77796a2c4b019f&search[name]=#{name}")
       response = HTTParty.get("http://index1.homeflow.co.uk/places", :query => {:api_key => "77467477edfd2689cd77796a2c4b019f", :"search[name]" => name})
       response.body
     end
 
     def properties(place_id, channel, min_price, max_price)
-      p min_price
-      p max_price
       query = {:api_key => "77467477edfd2689cd77796a2c4b019f", :"search[place][id]" => place_id, :"search[channel]" => channel}
       query[:"search[min_price]"] = min_price if min_price != nil
       query[:"search[max_price]"] = max_price if max_price != nil
-      p query
       response = HTTParty.get("http://index1.homeflow.co.uk/properties", :query => query)
-
-
-      #   ?api_key=77467477edfd2689cd77796a2c4b019f&search[place][id]=#{place_id}&search[channel]=#{channel}&search[min_price]=#{min_price}&search[max_price]=#{max_price}")
-      # # HTTParty.post("http://rubygems.org/api/v1/gems/httparty/owners",
-      # # :query => { :email => "alan+thinkvitamin@carsonified.com" })
-      # # :query => { :"search[place][id]" => place_id, :"search[channel]" =>  }
-
-
       response.body
     end
 
-    # def properties_choose(search_criter)
-    #
-    # end
-
   end
-
-  # 51e7c36573dadaf60fee9b4d
 
   api_request = APIRequest.new
 
@@ -56,14 +36,7 @@ class App < Sinatra::Application
     api_request.properties(params['place_id'], params['channel'], params['min_price'], params['max_price'])
   end
 
-  
+
   run! if app_file == $0
 
 end
-
-
-# url = localhost:4567/properties
-# localhost:4567/
-
-#
-# puts api_request.places
